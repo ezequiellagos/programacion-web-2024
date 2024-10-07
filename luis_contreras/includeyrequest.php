bf09a9366ba130c5b2f88ba0df051c7fec556080
Pans02 <robertocarrasco@alumnos.upla.cl>
2024-10-07 11:52:36 -0300

    Update includeyrequest.php

--- luis_contreras/includeyrequest.php
+++ luis_contreras/includeyrequest.php
@@ -1,3 +1,21 @@
 <?php
-    echo "Hola mundo desde php";
+    //Include y Require
+    // Incluir un archivo dentro de otro
+
+    /* 
+    Include: El include si el archivo no existe, el script sigue funcionando.
+
+    Require: Si el archivo no existe, el script se detiene.
+
+    include_once: Si el archivo ya fue incluido, no lo vuelve a incluir
+    
+    requiere_once: Si el archivo ya fue incluido, no lo vuelve a incluir
+
+
+    
+    */
+
+    include "proceso.php";
+
+    echo "test";
 ?>
