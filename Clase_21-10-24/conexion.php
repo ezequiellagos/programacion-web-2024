ea65aac7be3b23ef0e526251791e23e6ce3bcd40
Xeno <131300185+Diego-Ulpin12@users.noreply.github.com>
2024-10-21 13:27:03 -0300

    Update conexion.php

--- Clase_21-10-24/conexion.php
+++ Clase_21-10-24/conexion.php
@@ -14,3 +14,18 @@ $sql = "SELECT * FROM alumnos;";
 
 //Ejecutar consulta
 $resultado = mysqli_query($conexion, $sql);
+
+//Verificar si hay resultados
+if(mysqli_num_rows($resultado) > 0)
+{
+    //Mostrar los datos de cada fila
+    while($row = mysqli_fetch_assoc($resultado))
+    {
+        echo "id: " . $row["id"] . " - Nombre: " . $row["nombre"] . " - Apellido: " . $row["apellido"] . "<br>";
+    }
+}else{
+    echo "0 resultados";
+}
+//Cerrar conexion
+mysqli_close($conexion);
+
