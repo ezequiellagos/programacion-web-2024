de186c199bccb609023035d2047c745f5b360e1c
felipe <felipe.guasch@alumnos.upla.cl>
2024-10-21 11:18:50 -0300

    Felipe

---
+++ Felipe_Guasch/include y require.php
@@ -0,0 +1,16 @@
+<?php
+// include y Require
+// Incluir un archivo dentro de otro
+
+/*
+Include:
+require:
+include_once:
+require_once:
+*/
+
+include "proceso.php";
+
+echo suma(10, 5);
+
+?>
---
+++ Felipe_Guasch/proceso.php
@@ -0,0 +1,9 @@
+<?php
+
+echo"en archvio proceso <br>";
+
+function suma($a, $b){
+    return $a + $b;
+}
+
+?>
