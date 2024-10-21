416aa696c0fe60dd9e7acb69218c5a5fe28d6208
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2024-10-21 16:27:05 -0300

    Update conexion.php

--- 2024-10-21/conexion.php
+++ 2024-10-21/conexion.php
@@ -21,6 +21,41 @@ $conexion = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 // $sql = "SELECT * FROM alumnos;";
 
 // Ejecutar la consulta
-//$resultado = mysqli_query($conexion, $sql);
+$resultado = mysqli_query($conexion, $sql);
 
-//echo $resultado;
+// Verificar si hay resultados
+if (mysqli_num_rows($resultado) > 0) {
+    // Mostrar los datos de cada fila
+    while($row = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $row["id"] . " - Nombre: " . $row["nombre"] . " - Apellido: " . $row["apellido"] . "<br>";
+    }
+}else {
+    echo "0 resultados";
+}
+
+// Cerrar la conexion
+mysqli_close($conexion);
+
+
+// Crear conexión de manera orientda a objetos
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// Verificar conexion
+if ($conn->connect_error){
+    die("Conexion fallida: " . $conn->connect_error);
+}
+
+// Ejecutar la consulta
+$result = $conn->query($sql);
+
+// Verificar si hay resultados
+if ($result->num_rows > 0) {
+    foreach($result as $row){
+        echo "id: " . $row["id"] . " - Nombre: " . $row["nombre"] . " - Apellido: " . $row["apellido"] . "<br>";
+    }
+} else {
+    echo "0 resulatdos";
+}
+
+// Cerrar conexion
+$conn->close();
