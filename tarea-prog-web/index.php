cf2462c056d4b413b143e17cf396f93e4f6195f5
Bryan-Cardenas1 <bryan.cardenas@alumnos.upla.cl>
2024-11-04 05:58:03 -0300

    Formulario Terminado

--- tarea-prog-web/index.php
+++ tarea-prog-web/index.php
@@ -7,13 +7,11 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body class="text-bg-dark bg-dark">
-
-    <form action="procesar.php" method="post">
-    <div class="container justify-content-center text-center bg-secondary">
-        
+    <form action="procesar.php" method="POST">
+    <div class="container justify-content-center text-center">
             <div class="row-sm-12 mb-3">
             
-                <label  for="name">Nombres</label>
+                <label  for="name" class="form-label">Nombres</label>
                 <input  type="text" id="name" name="name">
 
             </div>
@@ -25,20 +23,22 @@
             </div>
             <div class="row-sm-12 mb-3">
                 
-                <label  for="nationality">Nacionalidad</label>
+                <label  for="nationality" class="form-label">Nacionalidad</label>
                 <input  type="text" id="nationality" name="nationality">
                 
             </div>
+            <div class="row-sm-12 mb-3">
+                <label for="Genero" class="form-label">Genero</label>
+                <select name="Genero">
+                    <option value="M">Masculino</option>
+                    <option value="F">Femenino</option>
+                </select>
+            </div>
             
-            <label for="Genero">Genero</label>
-            <select name="Genero">
-                <option value="M">Masculino</option>
-                <option value="F">Femenino</option>
-            </select>
 
             <div class="row-sm-12 mb-3">
         
-                <label  for="rut" >Rut</label>
+                <label  for="rut" class="form-label" >Rut</label>
                 <input  type="text" id="rut" name="rut" maxlength="9" placeholder="12345678-9" >
                 
             </div>
@@ -49,27 +49,35 @@
             
             <div class="row-sm-12 mb-3">
                 
-                <label  for="birthdate">Fecha de nacimiento</label>
+                <label  for="birthdate" class="form-label">Fecha de nacimiento</label>
                 <input  type="text" id="birthdate" name="birthdate">
                 
             </div>
 
             <div class="row-sm-12 mb-3">
                 
-                <label  for="carrer">Profesion</label>
+                <label  for="carrer" class="form-label">Profesion</label>
                 <input  type="text" id="carrer" name="carrer">
                 
             </div>
 
             <div class="row-sm-12 mb-3">
                 
-                <label  for="carrer">Profesion</label>
+                <label  for="carrer" class="form-label">Profesion</label>
                 <input  type="text" id="carrer" name="carrer">
                 
             </div>
+            
 
         
             <!-- foto de la persona -->
+            <div class="row-sm-12 mb-3">
+                    <label for="imagen" class="form-label">Imagen para Carnet</label>
+                    <input class="form-control" type="file" id="imagen" name="imagen">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
             <!-- select de la fecha -->
         
             <!-- fecha de emision   -->
