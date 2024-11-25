b4c159b72745d9834cc21c309df82f8e7cfe64e9
Vicho <vicalex.adriazola@gmail.com>
2024-11-25 12:36:47 -0300

    Inicio

---
+++ clase 2024-10-07/Tarea/Paginas principal/Menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Opciones</title>
+</head>
+<body>    
+
+<h6>Titulo de ejemplo</h6>
+
+<button type="button"> 
+    <a href="Registrar.php">Registrar</a>
+</button>
+<button type="button"></button>
+
+
+
+
+</body>
+</html>
